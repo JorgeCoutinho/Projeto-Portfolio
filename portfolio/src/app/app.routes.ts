36aec5bcd19fb2ec2b43476bcc72d788bf1df05a
Jorge Mikael <jmca.lic19@uea.edu.br>
2024-10-10 11:03:10 -0300

    Passos iniciais do projeto

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
+
+
+// Components Home
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
